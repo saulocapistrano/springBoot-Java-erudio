ea5de74d825011fca96260497deffe1b06c122ba
saulocapistrano <saulocapistrano@gmail.com>
2024-08-17 15:53:41 -0300

    First commit

---
+++ springBoot-Java-erudio/src/main/java/com/ifinit/erudio/springbootjavaerudio/ErudioApp.java
@@ -0,0 +1,13 @@
+package com.ifinit.erudio.springbootjavaerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErudioApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErudioApp.class, args);
+    }
+
+}
---
+++ springBoot-Java-erudio/src/test/java/com/ifinit/erudio/springbootjavaerudio/ErudioAppTests.java
@@ -0,0 +1,13 @@
+package com.ifinit.erudio.springbootjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErudioAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
